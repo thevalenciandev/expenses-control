007295ec718728246865004466beb8d04493efb7
thevalenciandev <thevalenciandev@gmail.com>
2019-10-13 15:08:17 +0100

    Passing export file name as an argument

--- main_yearly_stats.py
+++ main_yearly_stats.py
@@ -2,8 +2,12 @@ from labels import Label
 from santander_transactions_parser import get_transactions_from_txt_export
 from stats import YearlyStats
 
+import sys
+
 if __name__ == '__main__':
-    all_time_transactions = get_transactions_from_txt_export('C:/Temp/Santander_statement.txt')
+    export_file = sys.argv[1]
+    print(f'Processing export file {export_file}')
+    all_time_transactions = get_transactions_from_txt_export(export_file)
 
     # Yearly PnL per month
     YearlyStats('2019', all_time_transactions).print()
